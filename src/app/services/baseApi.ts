43b7ed5542da6f6e136246bf20142acd8f7f5fca
RocknRollBoss <rocknrollboss260@gmail.com>
2025-03-22 18:30:49 +0200

    gf

--- src/app/services/baseApi.ts
+++ src/app/services/baseApi.ts
@@ -4,14 +4,29 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   tagTypes: ["Team"],
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_API_URL,
-    credentials: "include",
+    baseUrl:
+      "https://mern-team-backend-pdayzbf2a-rocknrolls-projects.vercel.app",
+    credentials: "same-origin",
     prepareHeaders: headers => {
       const token = localStorage.getItem("token")
       if (token) {
-        headers.set("Authorization", token)
+        headers.set("Authorization", `Bearer ${token}`)
       }
+      return headers
     },
   }),
   endpoints: () => ({}),
 })
+
+/* 
+baseQuery: fetchBaseQuery({
+  baseUrl: "https://mern-team-backend-pdayzbf2a-rocknrolls-projects.vercel.app",
+  credentials: "same-origin", // Или временно 'omit'
+  prepareHeaders: headers => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+}) */
