2e2ff5332dfa41cc0eafd81c88fbdf75b4bf9ccc
RocknRollBoss <rocknrollboss260@gmail.com>
2025-03-22 16:18:06 +0200

    fix

--- src/app/services/baseApi.ts
+++ src/app/services/baseApi.ts
@@ -5,8 +5,7 @@ export const baseApi = createApi({
   tagTypes: ["Team"],
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_BASE_API_URL,
-    /*   credentials: "include", */
-    mode: "no-cors",
+    credentials: "include",
     prepareHeaders: headers => {
       const token = localStorage.getItem("token")
       if (token) {
